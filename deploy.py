c1a1d7cf509225940cf6b2a0c68d64be33101ad8
vanchhita <vanchhitarai2000@gmail.com>
2023-04-24 15:02:10 +0530

    file modified

--- deploy.py
+++ deploy.py
@@ -106,7 +106,7 @@ print(artifact_path)
 
 #Download the artifact in local machine
 
-model_path = mlflow.artifacts.download_artifacts(artifact_path)
+model_path = mlflow.artifacts.download_artifacts(artifact_path,dst_path="C:\\Users\v\Desktop\mlflow_demo\mlflow_s3bucket_demo\Models")
 
 print("MODEL PATH:",model_path)
 
