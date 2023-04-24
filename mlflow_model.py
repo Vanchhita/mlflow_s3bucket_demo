40b3341717072df5b3bef139c44132a97a308082
vanchhita <vanchhitarai2000@gmail.com>
2023-04-24 12:10:51 +0530

    file added

---
+++ mlflow_model.py
@@ -0,0 +1,80 @@
+
+from sklearn.model_selection import train_test_split 
+from sklearn.linear_model import LinearRegression 
+from sklearn import metrics 
+import os
+# from statistics import LinearRegression
+import warnings
+import sys
+
+import pandas as pd
+import numpy as np
+from sklearn.metrics import mean_squared_error, mean_absolute_error, r2_score
+
+
+from sklearn.linear_model import ElasticNet
+from urllib.parse import urlparse
+import mlflow
+
+
+import logging
+mlflow_uri="http://127.0.0.1:5000"
+mlflow.set_tracking_uri(mlflow_uri)
+logging.basicConfig(level=logging.WARN)
+logger = logging.getLogger(__name__)
+
+def eval_metrics(actual, pred):
+    rmse = np.sqrt(mean_squared_error(actual, pred))
+    mae = mean_absolute_error(actual, pred)
+    r2 = r2_score(actual, pred)
+    return rmse, mae, r2
+if __name__ == "__main__":
+    warnings.filterwarnings("ignore")
+    np.random.seed(40)
+
+    
+    
+    try:
+        
+        car_data = pd.read_csv('daTA.csv')
+        
+    except Exception as e:
+        logger.exception(
+            "Unable to download training & test CSV, check your internet connection. Error: %s", e
+        )
+        
+    car_data.replace({'Fuel_Type':{'Petrol':0,'Diesel':1,'CNG':2}},inplace=True) 
+    car_data.replace({'Seller_Type':{'Dealer':0,'Individual':1}},inplace=True) 
+    car_data.replace({'Transmission':{'Manual':0,'Automatic':1}},inplace=True) 
+    X = car_data.drop(['Car_Name','Selling_Price'],axis=1) 
+    Y = car_data['Selling_Price'] 
+    X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size = 0.2, random_state=42) 
+    alpha = float(sys.argv[1]) if len(sys.argv) > 1 else 0.5
+    l1_ratio = float(sys.argv[2]) if len(sys.argv) > 2 else 0.5
+    with mlflow.start_run():
+        #loading the linear regression model 
+        lr = LinearRegression() 
+        #Now we can fit the model to our dataset 
+        lr.fit(X_train,Y_train) 
+        # prediction on Training data 
+        test_data_prediction = lr.predict(X_test) 
+        (rmse, mae, r2) = eval_metrics(Y_test, test_data_prediction)
+        print("Elasticnet model (alpha={:f}, l1_ratio={:f}):".format(alpha, l1_ratio))
+        print("  RMSE: %s" % rmse)
+        print("  MAE: %s" % mae)
+        print("  R2: %s" % r2)
+
+        mlflow.log_param("alpha", alpha)
+        mlflow.log_param("l1_ratio", l1_ratio)
+        mlflow.log_metric("rmse", rmse)
+        mlflow.log_metric("r2", r2)
+        mlflow.log_metric("mae", mae)
+
+        tracking_url_type_store = urlparse(mlflow.get_tracking_uri()).scheme
+        # Model registry does not work with file store
+        if tracking_url_type_store != "file":
+            # Register the model
+            
+            mlflow.sklearn.log_model(lr, "model", registered_model_name="ElasticnetHourModel")
+        else:
+            mlflow.sklearn.log_model(lr, "model")
