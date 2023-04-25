ec894f752b19747a55cc47689e5850f73376472a
vanchhita <vanchhitarai2000@gmail.com>
2023-04-25 13:35:21 +0530

    file corrected

--- deploy.py
+++ deploy.py
@@ -109,8 +109,8 @@ print(artifact_path)
 #Download the artifact in local machine
 
 
-# run_id = "012f572be865428c9ab5b701ab2a6d1c"  # specify the ID of the run that contains the artifacts you want to download
-# run = mlflow.get_run(run_id)
+run_id = "012f572be865428c9ab5b701ab2a6d1c"  # specify the ID of the run that contains the artifacts you want to download
+run = mlflow.get_run(run_id)
 
 artifact_uri = run.info.artifact_uri
 # local_dir = "C:\\Users\v\Desktop\models"  # specify the path of the local directory where you want to save the downloaded artifacts
