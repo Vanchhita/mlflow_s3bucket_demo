d4a3e83de338f87bac13a512bb90fb23b404f0ce
vanchhita <vanchhitarai2000@gmail.com>
2023-04-27 11:33:04 +0530

    file modified

--- deploy.py
+++ deploy.py
@@ -65,7 +65,7 @@ print(artifact_path)
 #Download the artifact in local machine
 # model_path = mlflow.artifacts.download_artifacts(artifact_path,dst_path=os.environ['ARTIFACT_DESTINATION_PATH'])
 local_path = mlflow.artifacts.download_artifacts(run_id, "daTA.csv", dst_path=os.environ['ARTIFACT_DESTINATION_PATH'])
-print("MODEL PATH:",model_path)
+print("MODEL PATH=",model_path)
 
 
 ## USING BOTO3 ##
