ea89da8b433cde6212fc5f606b45d3d9b5d7a796
vanchhita <vanchhitarai2000@gmail.com>
2023-04-27 13:47:51 +0530

    file

--- deploy.py
+++ deploy.py
@@ -63,8 +63,9 @@ artifact_path = model_version_details.source
 print(artifact_path)
 
 #Download the artifact in local machine
-model_path = mlflow.artifacts.download_artifacts(artifact_path,dst_path=os.environ['ARTIFACT_DESTINATION_PATH'])
-# model_path = mlflow.artifacts.download_artifacts(run_id, dst_path=os.environ['ARTIFACT_DESTINATION_PATH'])
+# model_path = mlflow.artifacts.download_artifacts(artifact_path,dst_path=os.environ['ARTIFACT_DESTINATION_PATH'])
+model_path = mlflow.pyfunc.load_model(f"runs:/{run_id}/model")
+
 print("MODEL PATH=",model_path)
 
 
